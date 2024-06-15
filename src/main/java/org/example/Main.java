918716d2cfe647e0014ceafff2a1305dcbc7494e
olgakonieva <konievaolga@gmail.com>
2024-06-15 18:39:50 +0300

    I have created Enum with planets

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for(SolarSystem SolarSystem:SolarSystem.values()){
+            System.out.println(SolarSystem + " - " + SolarSystem.previous + " and " + SolarSystem.next);
+        }
+    }
+}

07213f4351466a711b21a9e7e27cc14b677e893c
olgakonieva <konievaolga@gmail.com>
2024-06-15 18:49:26 +0300

    I have created Enum with planets and constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         for(SolarSystem SolarSystem:SolarSystem.values()){
-            System.out.println(SolarSystem + " - " + SolarSystem.previous + " and " + SolarSystem.next);
+            System.out.println(SolarSystem + " - " + SolarSystem.previous + " and " + SolarSystem.next + "!");
         }
     }
 }

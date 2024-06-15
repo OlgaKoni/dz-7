076ebff9b5cb2d4f25791c8bafba566114d638c4
olgakonieva <konievaolga@gmail.com>
2024-06-15 18:43:43 +0300

    I have created Enum with planets and constructor

--- src/main/java/org/example/SolarSystem.java
+++ src/main/java/org/example/SolarSystem.java
@@ -4,7 +4,7 @@ public enum SolarSystem {
     MERCURY(1,0, 11, 201,"sun", "VENUS" ),
     VENUS(2,1, 22, 302, "MERCURY", "EARTH"),
     EARTH(3,2, 33, 309, "VENUS", "MARS"),
-    MARS(4,3,44, 250, "EARTH","JUPITER"),
+    MARS(4,3,44, 251, "EARTH","JUPITER"),
     JUPITER(5,4,55, 999, "MARS", "SATURN"),
     SATURN(6,5,66, 677, "JUPITER","URANUS" ),
     URANUS(7,6,77, 558, "SATURN", "NEPTUNE"),
